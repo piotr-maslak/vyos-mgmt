10ef131c60bc93645360db4f1450c5f8399017f5
Piotr Maslak <piotr@maslak.waw.pl>
2024-01-08 09:03:07 +0100

    Remove tty.bat

--- tty.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call config.cmd
-
-IF "%pw%" == "" (SET /p pw=enter password or press enter to use pagent:)
-IF NOT "%pw%" == "" (SET pw=-pw %pw%)
-IF "%pw%" == "" (SET pw=-agent)
-putty.exe -ssh -P %port% %pw% %user%@%host%
-pause
