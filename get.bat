1beb43fa85840af0f9538e0c1f9a7b908e2309ba
Piotr Maslak <piotr@maslak.waw.pl>
2024-01-09 16:32:13 +0100

    add get.bat with version

--- get.bat
+++ get.bat
@@ -11,17 +11,17 @@ IF "%pw%" == "" (SET pw=-agent)
 
 echo get from %host%
 echo get configuration          (plink -batch -P %port% %pw% %user%@%host% -m get-scripts\configuration.sh)
-plink -batch -P %port% %pw% %user%@%host% -m get-scripts\configuration.sh > configuration.cfg
+plink -batch -P %port% %pw% %user%@%host% -m get-scripts\%version%\configuration.sh > configuration.cfg
 echo get completed, wait %timeout%s
 ping localhost -n %timeout% >NUL
 echo get configuration-commands (plink -batch -P %port% %pw% %user%@%host% -m get-scripts\commands.sh)
-plink -batch -P %port% %pw% %user%@%host% -m get-scripts\commands.sh > commands.cfg
+plink -batch -P %port% %pw% %user%@%host% -m get-scripts\%version%\commands.sh > commands.cfg
 echo get completed, wait %timeout%s
 ping localhost -n %timeout% >NUL
 echo get firewall-rules         (plink -batch -P %port% %pw% %user%@%host% -m get-scripts\firewall.sh)
-plink -batch -P %port% %pw% %user%@%host% -m get-scripts\firewall.sh > firewall.txt
+plink -batch -P %port% %pw% %user%@%host% -m get-scripts\%version%\firewall.sh > firewall.txt
 echo get completed, wait %timeout%s
 ping localhost -n %timeout% >NUL
 echo get nat-rules              (plink -batch -P %port% %pw% %user%@%host% -m get-scripts\nat.sh)
-plink -batch -P %port% %pw% %user%@%host% -m get-scripts\nat.sh > nat.txt
+plink -batch -P %port% %pw% %user%@%host% -m get-scripts\%version%\nat.sh > nat.txt
 echo get completed, wait %timeout%s
