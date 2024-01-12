d0970c738aabf7a5bf0fe1c81bc615ac35484b53
Piotr Maslak <piotr@maslak.waw.pl>
2024-01-12 15:01:34 +0100

    Scan.bat

--- scan.bat
+++ scan.bat
@@ -6,10 +6,4 @@ if exist config.cmd (
     call ..\config.cmd
 )
 
-if exist include (
-    call include\include.cmd
-) else (
-    call ..\include\config.cmd
-)
-
-nmap -sS -sU -T4 -A -v %host%
+nmap %host%
