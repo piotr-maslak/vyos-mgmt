2646ed41116b1d63d1f246fdf0917a7a1bd25e38
Piotr Maslak <piotr@maslak.waw.pl>
2024-10-29 10:16:22 +0100

    nopass parameter added

--- Get-Configuration.ps1
+++ Get-Configuration.ps1
@@ -1,19 +1,27 @@
 Param (  # Parameters
     [String]$Hostname, # -Hostname fqdn.example.com
-    [String]$Username # -Username username
+    [String]$Username, # -Username username
+    [switch]$nopass
 )
 
 . .\include-scripts\SettingsManager.ps1
 $System = [SettingsManager]::new()
 $Credentials = [CredentialsManager]::new()
 
+#plinkCmd
+$plinkCmd = "plink.exe -batch"
+$plinkCmd += " -P " + $System.Settings.Get("Port")
+if(-not $nopass){
+$plinkCmd += " -pw " + $Credentials.GetPassword()
+}
+$plinkCmd += " " + $Credentials.GetUsername() + "@" + $System.Settings.Get("Hostname")
 
-
-$getConfigurationCmd = "plink.exe -batch -P " + $System.Settings.Get("Port") + " -pw " + $Credentials.GetPassword() + " " + $Credentials.GetUsername() + "@" + $System.Settings.Get("Hostname") +" -m include-scripts\" + $System.Settings.Get("System")+ "\configuration.sh"
-$getCommandsCmd = "plink.exe -batch -P " + $System.Settings.Get("Port") + " -pw " + $Credentials.GetPassword() + " " + $Credentials.GetUsername() + "@" + $System.Settings.Get("Hostname") +" -m include-scripts\" + $System.Settings.Get("System")+ "\commands.sh"
+$getConfigurationCmd = $plinkCmd + " -m include-scripts\" + $System.Settings.Get("System") + "\configuration.sh"
 Write-Output $getConfigurationCmd
-Write-Output $getCommandsCmd
 Invoke-Expression -Command $getConfigurationCmd | Out-File -FilePath "configuration.txt"
+$getCommandsCmd = $plinkCmd + " -m include-scripts\" + $System.Settings.Get("System") + "\commands.sh"
+Write-Output $getCommandsCmd
 Invoke-Expression -Command $getCommandsCmd | Out-File -FilePath "commands.txt"
+
 $System.Save()
 $Credentials.Save()
